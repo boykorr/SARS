9d1e6ee8362688092acfdb86c97ade9170f4a9f2
2130056r <boykoradulov@yahoo.com>
2016-03-09 16:11:28 +0000

    first commit

---
+++ SARS_project/SARS/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import patterns, url
+from SARS import views
+from django.conf import settings
+
+urlpatterns = patterns('',
+    url(r'^$', views.query_construction, name='query_construction'),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^login/$', views.user_login, name='login'),
+    url(r'^logout/$', views.user_logout, name='logout'),
+    )
+if not settings.DEBUG:
+        urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
