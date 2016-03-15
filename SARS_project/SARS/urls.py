febf7142a42d1802e454772e9e1481ef6d6d08c8
2130056r <boykoradulov@yahoo.com>
2016-03-15 17:04:31 +0000

    cleared database nearly got the user files working

--- SARS_project/SARS/urls.py
+++ SARS_project/SARS/urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns('',
     #url(r'^login/$', views.user_login, name='login'),
     #url(r'^logout/$', views.user_logout, name='logout'),
     url(r'^abstractevaluation/$', views.abstract_evaluation, name='abstract_evaluation'),
-    url(R'^accounts/register/$', views.successful_registration, name='registration_register'),
+    url(r'^successfulregistration/$', views.successful_registration, name='registration_register'),
     )
 if not settings.DEBUG:
         urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
