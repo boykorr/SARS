8dd3656ccb9cfbfab35a122849d5c8f1feeb0728
2035079b <2035079b@student.gla.ac.uk>
2016-03-16 11:54:55 +0000

    Fixed the fucking username problem

--- SARS_project/SARS/views.py
+++ SARS_project/SARS/views.py
@@ -1,11 +1,13 @@
 from django.db.models import query
 from django.shortcuts import render
-from SARS.models import Query
+from SARS.models import Query, UserProfile
 from SARS.forms import UserForm, UserProfileForm, QueryForm
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.decorators import login_required
 import os
+from django.contrib.auth.models import User
+from datetime import datetime
 
 printQuery = []
 
@@ -16,6 +18,7 @@ def query_construction(request):
     if getQueryRequest != "" and getQueryRequest != None and getQueryRequest not in printQuery:
         printQuery.append(str(getQueryRequest))
     context_dict = {'form':form, 'query':printQuery}
+    #print User.objects.all().values_list('username')[len(User.objects.all().values_list('username'))-1][0]
     return render(request, 'SARS/query_construction.html', context_dict)
 
 def abstract_evaluation(request):
@@ -23,11 +26,22 @@ def abstract_evaluation(request):
     return HttpResponse(printQuery)
 
 def successful_registration(request):
+    #if request.session.test_cookie_worked():
+        #print ">>>> TEST COOKIE WORKED!"
+        #request.session.delete_test_cookie()
+        #print request.COOKIES
+
+
 
     path = 'H:\Github\SARS\SARS_project\userQueries'
-    currentUsername = "bil.txt"
+    currentUsername = User.objects.all().values_list('username')[len(User.objects.all().values_list('username'))-1][0] + ".txt"
     file = os.path.join(path, currentUsername)
     queryFile = open(file,"w")
+    #userForm = QueryForm()
+    #print userForm.fields['user']
+    #print userForm.user.get_username()
+    #response.set_cookie('yer_maw', datetime.now())
+    #print request.COOKIES
 
     return HttpResponseRedirect('/SARS/')
 
