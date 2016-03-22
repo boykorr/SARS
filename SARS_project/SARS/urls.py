81160d44cf15c9c38c921eec2ff1caea0080536e
ChunnyBunny3817 <aaron.cheng@live.co.uk>
2016-03-22 15:46:54 +0000

    index partial revert

--- SARS_project/SARS/urls.py
+++ SARS_project/SARS/urls.py
@@ -4,9 +4,9 @@ from django.conf import settings
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^basic_search/$', views.basic_query, name='basic_search'),
-    url(r'^advanced_search/$', views.advanced_query, name='advanced_search'),
-    url(r'^user_guide/$', views.user_guide, name='user_guide'),
+    url(r'^basicsearch/$', views.basic_query, name='basic_search'),
+    url(r'^advancedsearch/$', views.advanced_query, name='advanced_search'),
+    url(r'^userguide/$', views.user_guide, name='user_guide'),
     #url(r'^register/$', views.register, name='register'),
     #url(r'^login/$', views.user_login, name='login'),
     #url(r'^logout/$', views.user_logout, name='logout'),
